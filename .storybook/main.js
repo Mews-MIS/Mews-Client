6268977b744cb39f2dbc2394a49db4a6125272d9
Choi siun <siun0331@gmail.com>
2023-01-09 20:26:25 +0900

    chore: emotion + storybook 세팅 #1

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,13 +11,14 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "storybook-addon-react-router-v6",
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
   webpackFinal: async (config) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules", "styles"];
+
     config.resolve.alias["@public"] = path.resolve(__dirname, "../public/");
     config.resolve.alias["@src"] = path.resolve(__dirname, "../src/");
     config.resolve.alias["@utils"] = path.resolve(__dirname, "../src/utils/");
---
+++ src/components/ContentCard/index.tsx
@@ -0,0 +1,9 @@
+const ContentCard = () => {
+  return (
+    <div>
+      <p>시운</p>
+    </div>
+  );
+};
+
+export default ContentCard;

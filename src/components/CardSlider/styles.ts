cedf51a9a548ac52a471fc8eb30bf659cd99082b
Choi siun <siun0331@gmail.com>
2023-01-10 13:52:36 +0900

    feat: core - 공통컴포넌트 카드슬라이더 제작

---
+++ src/components/CardSlider/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  padding: 20px;
+  gap: 20px;
+  display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
+`;

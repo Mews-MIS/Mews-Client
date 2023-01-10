cb49b37fb529f65239a94b057d02387dffa8c625
Choi siun <siun0331@gmail.com>
2023-01-10 13:52:36 +0900

    feat: core - 공통컴포넌트 카드슬라이더 제작

---
+++ src/components/CardSlider/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as s from "./styles";
+
+const CardsSlider = ({ children }: { children: React.ReactNode }) => {
+  return <s.Wrapper>{children}</s.Wrapper>;
+};
+
+export default CardsSlider;
--- src/components/ContentCard/index.tsx
+++ src/components/ContentCard/index.tsx
@@ -1,5 +1,5 @@
 import * as s from "./styles";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import BookmarkBtn from "@public/button/BookmarkBtn.svg";
 import LikeBtn from "@public/button/LikeBtn.svg";
 const ContentCard = () => {

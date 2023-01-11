5f55589d11be4b8004c8b61aaa670f664d0bc113
Sangjun Park <sangjun1997@naver.com>
2023-01-12 01:35:26 +0900

    feat: TopNavBar 컴포넌트 작업 resolves #8

---
+++ src/components/TopNavBar/index.tsx
@@ -0,0 +1,43 @@
+import * as s from "./styles";
+import Logo from "@public/icon/Logo.svg";
+import SearchIcon from "@public/icon/SearchIcon.svg";
+import Hamberger from "@public/icon/Hamberger.svg";
+
+const TobNavBar = () => {
+
+
+    const onClickLogo = () => {
+        console.log("메인 페이지로 라우팅");
+    };
+
+    const onClickSearch = () => {
+        console.log("검색 페이지로 라우팅");
+    };
+
+    const onClickHamberger = () => {
+        console.log("메뉴 페이지 렌더링");
+    };
+
+    return (
+        <s.Wrapper>
+            <s.NavContainer>
+                    <s.NavLeftIcon onClick={onClickLogo}>
+                        <Logo />
+                    </s.NavLeftIcon>
+
+                    <s.NavRightIcons>
+                            <s.NavSearch onClick={onClickSearch}>
+                                <SearchIcon />
+                            </s.NavSearch>
+
+                            <s.NavHamberger onClick={onClickHamberger}>
+                                <Hamberger />
+                            </s.NavHamberger>
+                    </s.NavRightIcons>
+            </s.NavContainer>
+
+        </s.Wrapper>
+    )
+}
+
+export default TobNavBar;

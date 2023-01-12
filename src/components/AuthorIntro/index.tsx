5e58f3ae7c062fda6080d7c626dea9d7f1ac61af
Choi siun <siun0331@gmail.com>
2023-01-12 20:10:59 +0900

    feat: 공통 컨테이너 제작 및 스토리 코드 추가

--- src/components/AuthorIntro/index.tsx
+++ src/components/AuthorIntro/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as s from "./styles";
 
 function AuthorIntro() {
-  //더미 데이터
+  // 더미 데이터
   const authorName = "이정우";
   const intro = "꿈은 없고, 계속 놀고만 싶습니다.";
 
@@ -10,7 +10,7 @@ function AuthorIntro() {
     <s.ProfileContainer>
       <s.ProfileBox>
         <s.ProfileImage />
-        {/** 해당 부분 프로필 이미지 **/}
+        {/** 해당 부분 프로필 이미지 * */}
         <s.ProfileTextLabel>
           <s.ProfileAuthorName>{authorName}</s.ProfileAuthorName>
           <s.ProfileIntro>{intro}</s.ProfileIntro>

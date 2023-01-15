c409478e514e6f9a123fc95827649c76a30920a8
soojeong <tnwjd7890@gmail.com>
2023-01-15 18:00:01 +0900

    feat: bottom Navbar layout and route #12

--- src/components/BottomNavbar/index.tsx
+++ src/components/BottomNavbar/index.tsx
@@ -1,27 +1,40 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import * as s from "./styles";
 import MainIcon from "@public/icon/MainIcon.svg";
 import NewsIcon from "@public/icon/NewsIcon.svg";
 import CalendarIcon from "@public/icon/CalendarIcon.svg";
 import MyPageIcon from "@public/icon/MyPageIcon.svg";
 
-const BottomNavbar = () => {
+const BottomNavBar = () => {
+  const router = useRouter();
+
+  //각 페이지에 맞는 라우팅 연결 필요. 현재 상태는 아이콘 모두 active되어있는 상태
   return (
     <s.BtmNavContainer>
-      <s.BtmNavItem>
-        <MainIcon />
-      </s.BtmNavItem>
-      <s.BtmNavItem>
-        <NewsIcon />
-      </s.BtmNavItem>
-      <s.BtmNavItem>
-        <CalendarIcon />
-      </s.BtmNavItem>
-      <s.BtmNavItem>
-        <MyPageIcon />
-      </s.BtmNavItem>
+      <Link href="/">
+        <s.BtmNavItem className={router.pathname === "/" ? "active" : ""}>
+          <MainIcon />
+        </s.BtmNavItem>
+      </Link>
+      <Link href="/">
+        <s.BtmNavItem className={router.pathname === "/" ? "active" : ""}>
+          <NewsIcon />
+        </s.BtmNavItem>
+      </Link>
+      <Link href="/">
+        <s.BtmNavItem className={router.pathname === "/" ? "active" : ""}>
+          <CalendarIcon />
+        </s.BtmNavItem>
+      </Link>
+      <Link href="/">
+        <s.BtmNavItem className={router.pathname === "/" ? "active" : ""}>
+          <MyPageIcon />
+        </s.BtmNavItem>
+      </Link>
     </s.BtmNavContainer>
   );
 };
 
-export default BottomNavbar;
+export default BottomNavBar;

f7dd18611b9641c78f24741b9d716fb5f2810edc
Dahyeonni <youlove7r@gmail.com>
2023-01-21 03:07:15 +0900

    feat: 전체 디자인 추가 #20

--- src/pages/notlogin/styles.ts
+++ src/pages/notlogin/styles.ts
@@ -2,14 +2,50 @@ import styled from "@emotion/styled";
 import theme from "@styles/Theme";
 
 export const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
   display: flex;
   width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const NotLoginContainer = styled.div``;
+export const NotLoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-direction: column;
+  width: 80%;
+  height: 50%;
+`;
 
-export const MewsIntro = styled.div``;
+export const MewsIntro = styled.div`
+  font-size: ${theme.FONT_SIZE.TITLE_SIZE};
+  font-weight: ${theme.FONT_WEIGHT.BOLD};
+  color: ${theme.COLORS.BRIGHT_ORANGE};
+`;
 
-export const MewsAd = styled.div``;
+export const MewsAd = styled.div`
+  font-size: ${theme.FONT_SIZE.MAIN_SIZE};
+  color: ${theme.COLORS.LINE_GRAY};
+  font-weight: ${theme.FONT_WEIGHT.DEFAULT};
+`;
 
-export const GoogleBtn = styled.div``;
+export const Button = styled.button`
+  width: 100%;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  font-size: ${theme.FONT_SIZE.MAIN_SIZE};
+  background-color: ${theme.COLORS.CONTAINER_WHITE};
+  color: ${theme.COLORS.LINE_GRAY};
+  padding: 4.5%;
+  -webkit-box-shadow: 1px 4px 3px 0px rgba(193, 193, 193, 1);
+  -moz-box-shadow: 1px 4px 3px 0px rgba(193, 193, 193, 1);
+  box-shadow: 1px 4px 3px 0px rgba(193, 193, 193, 1);
+`;

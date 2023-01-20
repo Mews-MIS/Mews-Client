393a972b7709c0a65327ed4afca5a524a4306b64
sangjun-Park1208 <sangjun1997@naver.com>
2023-01-20 14:31:09 +0900

    feat: 프로필 이미지 수정 컴포넌트

---
+++ src/components/EditProfileImage/EditProfileImage.stories.tsx
@@ -0,0 +1,8 @@
+import EditProfileImage from "@components/EditProfileImage";
+
+export default {
+  title: "component/EditProfileImage",
+  component: EditProfileImage,
+};
+
+export const editProfileImage = () => <EditProfileImage />;

f6c9ffa3ffea308758879af025eb81f9c17ad49c
Dahyeonni <youlove7r@gmail.com>
2023-01-20 19:05:06 +0900

    feat: 비로그인시 마이페이지 전체적인 틀 구성 #20

---
+++ src/pages/notlogin/styles.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import theme from "@styles/Theme";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const NotLoginContainer = styled.div``;
+
+export const MewsIntro = styled.div``;
+
+export const MewsAd = styled.div``;
+
+export const GoogleBtn = styled.div``;

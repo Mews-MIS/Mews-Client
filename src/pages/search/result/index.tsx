36cd2d159bd0ec450b6a93cca96152a4c2a70e28
Choi siun <siun0331@gmail.com>
2023-01-25 00:48:23 +0900

    feat: 검색 결과페이지 일부 구현

--- src/pages/search/result/index.tsx
+++ src/pages/search/result/index.tsx
@@ -1,4 +1,13 @@
-const SearchedResultPage = () => {
+// import fetch from "node-fetch";
+//
+// export async function getServerSideProps(context) {
+//   const page = (parseInt(context.query.page, 10) || "").toString();
+//
+//   const res = await fetch("주소");
+//   const product = await res.json();
+//   return { props: { searchResult: product.results } };
+// }
+const SearchedResultPage = (props: any) => {
   return <h1>검색 결과 페이지</h1>;
 };
 

324c7197fbb1c2b684547280115cd9ef23edd870
soojeong <tnwjd7890@gmail.com>
2023-01-27 14:56:32 +0900

    feat:작성자 페이지 제작 #34

--- src/pages/authorInfo/index.tsx
+++ src/pages/authorInfo/index.tsx
@@ -1,26 +1,58 @@
 import React from "react";
-import { useState } from "react";
-import TobNavBar from "@components/TopNavBar";
+import { useState, useEffect } from "react";
 import AuthorIntro from "@components/AuthorIntro";
 import ContentCard from "@components/ContentCard";
 import ContentWrapper from "@components/ContentWrapper";
 import CardSlider from "@components/CardSlider";
-import mySubscribeArticle from "@pages/tmp/mySubscribeArticle";
-import { Article, CurationType } from "../../types/article";
+import ContentRow from "@components/ContentRow";
+import infiniteArticle from "@pages/tmp/infiniteArticle";
+import { Article } from "../../types/article";
 import * as s from "./styles";
+
+// 추후 authorName은 props로 받아오는것으로 수정되어야 함
+interface textSetIf {
+  latestPost: string;
+  allPost: string;
+  authorName: string;
+}
 
 export default function AuthorInfo() {
   const [subscribe, setSubscribe] = useState<Boolean>(false);
-  const authorName = "한수정";
-  const countPost = 12;
+  const [array, setArray] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    getArticleList();
+  }, []);
+
+  const textSet: textSetIf = {
+    latestPost: "님이 최근 작성한 글",
+    allPost: "님이 작성한 글",
+    authorName: "한수정",
+  };
+
+  //추후 구독유무 넘겨주는 코드 추가되어야 함
   const onClickSubscribe = () => {
     setSubscribe(!subscribe);
+  };
+
+  //임시 데이터 불러옴
+  const getArticleList = () => {
+    const data = infiniteArticle.map((e: Article, index: number) => {
+      return (
+        <ContentRow
+          index={index + 1 + array.length}
+          contentInfo={{
+            ...e,
+          }}
+        />
+      );
+    });
+    setArray([...array, ...data]);
   };
 
   return (
     <div>
       <s.Wrapper>
-        <TobNavBar />
         <s.TopContainer>
           <AuthorIntro />
           <s.SubscribeBtnContainer onClick={onClickSubscribe}>
@@ -29,26 +61,24 @@ export default function AuthorInfo() {
             </s.SubscribeBtn>
           </s.SubscribeBtnContainer>
         </s.TopContainer>
+        <s.AllPostContainer>
+          <s.AllPost>{textSet.authorName + textSet.allPost}</s.AllPost>
+          <s.CountPost>{infiniteArticle.length}개</s.CountPost>
+        </s.AllPostContainer>
+        <s.BottomContainer>
+          <ContentWrapper contentName={textSet.authorName + textSet.latestPost}>
+            <CardSlider>
+              {infiniteArticle.slice(0, 3).map((e: Article) => {
+                return <ContentCard {...e} />;
+              })}
+            </CardSlider>
+          </ContentWrapper>
+          <ContentWrapper contentName={textSet.authorName + textSet.allPost}>
+            {infiniteArticle.length ? array : "게시글이 없습니다."}
+            <button onClick={() => getArticleList()}>더 불러오기</button>
+          </ContentWrapper>
+        </s.BottomContainer>
       </s.Wrapper>
-      <s.BottomContainer>
-        <ContentWrapper contentName={authorName}>
-          <CardSlider>
-            {mySubscribeArticle.map((e: Article) => {
-              return (
-                <ContentCard
-                  key={e.id}
-                  category={e.category}
-                  title={e.title}
-                  authorNames={e.authorNames}
-                  isActive={e.isActive}
-                  isLike={e.isLike}
-                  likeNum={e.likeNum}
-                />
-              );
-            })}
-          </CardSlider>
-        </ContentWrapper>
-      </s.BottomContainer>
     </div>
   );
 }
--- src/pages/authorInfo/styles.ts
+++ src/pages/authorInfo/styles.ts
@@ -1,18 +1,29 @@
 import styled from "@emotion/styled";
 import theme from "@styles/Theme";
 
-export const Wrapper = styled.div``;
+const defaultVal = {
+  defaultPadding: "0 5vw",
+};
+
+export const Wrapper = styled.div`
+  background-color: ${theme.COLORS.CONTAINER_WHITE};
+  padding: ${defaultVal.defaultPadding};
+`;
 
 export const TopContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 70px;
   background: ${theme.COLORS.CONTAINER_WHITE};
 `;
 
-export const SubscribeBtnContainer = styled.div``;
+export const SubscribeBtnContainer = styled.div`
+  color: ${theme.COLORS.FONT_YELLOW};
+  font-size: ${theme.FONT_SIZE.SMALL_SIZE};
+
+  margin-bottom: 5%;
+`;
 
 export const SubscribeBtn = styled.button`
   width: 85vw;
@@ -28,5 +39,19 @@ export const SubscribeBtn = styled.button`
     background: ${theme.COLORS.LINE_GRAY};
   }
 `;
-
+export const AllPostContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+  font-size: ${theme.FONT_SIZE.SUB_TITLE_SIZE};
+  background: ${theme.COLORS.CONTAINER_WHITE};
+  padding-bottom: 15px;
+  border-bottom: 1px solid ${theme.COLORS.BRIGHT_ORANGE};
+`;
+export const AllPost = styled.div`
+  padding-right: 8px;
+`;
+export const CountPost = styled.div`
+  color: ${theme.COLORS.BRIGHT_ORANGE};
+`;
 export const BottomContainer = styled.div``;

726afabc848b0dccc923c1199c44e4d07784d866
Choi siun <siun0331@gmail.com>
2023-01-27 23:46:03 +0900

    feat: 공통 레이아웃 적용 #38

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,16 @@ import GlobalStyles from "@styles/GlobalStyles";
 import { ThemeProvider } from "@emotion/react";
 import theme from "@styles/Theme";
 import { SessionProvider } from "next-auth/react";
+import Layout from "@components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </SessionProvider>
     </ThemeProvider>
   );

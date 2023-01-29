a24d96d76745c1af0c57dd31e7983ea4219feb32
Sangjun Park <sangjun1997@naver.com>
2023-01-30 02:26:01 +0900

    feat: ScheduleCategory 컴포넌트 추가

---
+++ src/components/ScheduleCategory/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as s from "./styles";
+
+const ScheduleCategory = () => {
+  return (
+    <s.Wrapper>ScheduleCategory</s.Wrapper>
+  )
+};
+
+export default ScheduleCategory;

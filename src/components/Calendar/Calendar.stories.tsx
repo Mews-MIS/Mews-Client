96edcec3a65e5ea8333161f519b7fc8c4ee4f992
Sangjun Park <sangjun1997@naver.com>
2023-01-30 02:25:01 +0900

    feat: Calendar 컴포넌트 추가

---
+++ src/components/Calendar/Calendar.stories.tsx
@@ -0,0 +1,8 @@
+import Calendar from "@components/Calendar";
+
+export default {
+    title: "component/Calendar",
+    component: Calendar,
+};
+
+export const calendar = () => <Calendar />;
---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as s from "./styles";
+
+const Calendar = () => {
+  return (
+    <s.Wrapper>Calendar</s.Wrapper>
+  )
+};
+
+export default Calendar;

4fdd7c205dc2716c1d177a750d63fa2d56e08e72
Sangjun Park <sangjun1997@naver.com>
2023-01-30 02:26:17 +0900

    feat: ScheduleList 컴포넌트 추가

---
+++ src/components/ScheduleList/ScheduleList.stories.tsx
@@ -0,0 +1,8 @@
+import ScheduleList from "@components/ScheduleList";
+
+export default {
+    title: "component/ScheduleList",
+    component: ScheduleList,
+};
+
+export const scheduleList = () => <ScheduleList />;
---
+++ src/components/ScheduleList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as s from "./styles";
+
+const ScheduleList = () => {
+  return (
+    <s.Wrapper>ScheduleList</s.Wrapper>
+  )
+};
+
+export default ScheduleList;

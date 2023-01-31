3b71abd0a700386f0c1ab3f61bbf7bb8055ec4a1
Sangjun Park <sangjun1997@naver.com>
2023-02-01 03:09:26 +0900

    feat: 학사일정 월 이동 컴포넌트, 각 날짜 컴포넌트 추가

--- src/components/DayItem/styles.ts
+++ src/components/DayItem/styles.ts
@@ -1,4 +1,31 @@
 import styled from "@emotion/styled";
 import theme from "@styles/Theme";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DayText = styled.div`
+  font-size: ${theme.FONT_SIZE.TITLE_SIZE};
+`;
+
+export const CategoryCircles = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
+
+export const Circle = styled.div`
+  background-color: ${props => props.color || "gray"};
+  width: 13px;
+  height: 13px;
+  border-radius: 50px;
+`;
--- src/components/MoveMonth/styles.ts
+++ src/components/MoveMonth/styles.ts
@@ -1,4 +1,22 @@
 import styled from "@emotion/styled";
 import theme from "@styles/Theme";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const BtnWrapper = styled.div`
+  cursor: pointer;
+`;
+
+export const CurrentMonthText = styled.div``;

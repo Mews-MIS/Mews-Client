be963249ce5f7bfb684fcb1b0389c179b8cb7073
Dahyeonni <youlove7r@gmail.com>
2023-02-05 16:22:37 +0900

    eslint

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/button-has-type */
 import React from "react";
 import * as s from "./styles";
 
--- src/pages/newspage/index.tsx
+++ src/pages/newspage/index.tsx
@@ -6,27 +6,22 @@ import Pagination from "@components/Pagination";
 import * as s from "./styles";
 
 const NewsPage = () => {
-  const [posts, setPosts] = useState<NewsListIcon[]>([]);
-
-  useEffect(() => {
-    getPosts();
-  }, []);
-
-  const getPosts = () => {
-    setPosts([...posts, ...NewsList]);
-  };
-
+  const [lists, setLists] = useState<NewsListIcon[]>([]);
   const [limit, setLimit] = useState(10); // 한 페이지에 보여줄 데이터 개수
   const [page, setPage] = useState(1); // 페이지 초기값은 1페이지
   const [counts, setCounts] = useState(1); // 데이터의 총 개수를 setCounts에 저장해서 사용
+  useEffect(() => {
+    setLimit(10);
+    setCounts(NewsList.length);
+  }, []);
   const [blockNum, setBlockNum] = useState(0); // 한페이지에 보여줄 페이지 네이션 개수를 block으로 지정하는 state
 
   return (
     <s.Wrapper>
       <s.NewsbottomContainer>
         <s.NewsListBox>
-          {posts.length
-            ? posts.map((element: NewsListIcon, index: number) => {
+          {NewsList.length
+            ? NewsList.slice(0, 10).map((element: NewsListIcon, index: number) => {
                 return <NewsListItem key={element.id} index={index + 1} contentInfo={element} />;
               })
             : "등록된 게시물이 없습니다"}

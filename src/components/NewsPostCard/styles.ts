81684977063a92a240739e9625304078cd1cced5
soojeong <tnwjd7890@gmail.com>
2023-03-09 02:45:44 +0900

    chore:category div의 min-width 제거

--- src/components/NewsPostCard/styles.ts
+++ src/components/NewsPostCard/styles.ts
@@ -12,7 +12,7 @@ export const Wrapper = styled.div`
 `;
 
 export const ContentContainer = styled.div`
-  width: 100%;
+  width: 90%;
   height: 100%;
   display: flex;
   flex-direction: column;
@@ -20,6 +20,7 @@ export const ContentContainer = styled.div`
 `;
 
 export const ImageContainer = styled.div`
+  display: flex;
   margin: 0 auto 15px auto; 
   height: 45vw;
   width: 100%;
@@ -32,21 +33,24 @@ export const ImageContainer = styled.div`
 `;
 
 export const NewsInfoSummary = styled.div`
-  margin: 0 auto;
-  width: 80vw;
-  min-width: 340px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
   padding: 0 2vw;
 `;
 
 export const NewsTitleLabel = styled.p`
+  display: flex;
+  justify-content: flex-start;
   font-size: ${theme.FONT_SIZE.SUB_TITLE_SIZE};
   font-weight: ${theme.FONT_WEIGHT.BOLD};
   margin-bottom: 7%;
 `;
 
 export const NewsCategoryLabel = styled.p`
+  display: flex;
+  justify-content: flex-end;
   color: ${theme.COLORS.FONT_YELLOW};
   font-size: ${theme.FONT_SIZE.SMALL_SIZE};
-  text-align: right;
   margin-bottom: 7%;
 `;

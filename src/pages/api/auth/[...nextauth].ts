bc88d62412d6a37fec7090a0321394cc029f83e3
Choi siun <siun0331@gmail.com>
2023-03-13 20:34:39 +0900

    feat: 쿠키 데이터 생성

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,34 +3,6 @@ import GoogleProvider from "next-auth/providers/google";
 import UserAPI from "@api/UserAPI";
 import { setCookie } from "nookies";
 
-type User = {
-  user: {
-    email?: string;
-    name?: string;
-    image?: string;
-  };
-};
-
-type Account = {
-  provider?: string;
-  id?: string;
-};
-
-type Profile = Record<string, unknown>;
-
-type JWT = {
-  email?: string;
-  name?: string;
-  [key: string]: any;
-};
-
-type Session = {
-  user: {
-    email?: string;
-    name?: string;
-  };
-};
-
 const callbacks = {
   signIn: async (params: any): Promise<SignInCallback> => {
     const { email, name, image } = params.user;
@@ -38,18 +10,14 @@ const callbacks = {
       const response = await UserAPI.googleLogin({
         userEmail: email,
         userName: name,
-        imgUrl: "",
+        imgUrl: image,
         introduction: "",
       });
-      console.log(response);
 
       const token = response.atk;
       setCookie(null, "access_token", token, {
         maxAge: 30 * 24 * 60 * 60, // 30일간 유지
-        path: "/",
       });
-
-      console.log(response);
 
       return true;
     } catch (error) {
@@ -57,9 +25,8 @@ const callbacks = {
       throw new Error("Unable to sign in");
     }
   },
-  async session({ session, token, user }) {
+  async session({ session, token }) {
     // Send properties to the client, like an access_token from a provider.
-
     return {
       ...session,
       accessToken: token.accessToken,
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,11 +4,14 @@ import TopNavBar from "@components/TopNavBar";
 import BottomNavBar from "@components/BottomNavbar";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import GLogo from "@public/icon/GLogo.svg";
 import * as s from "./styles";
 
 const NotLoginPage = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
+
+  const getSignIn = () => {
+    signIn("google", { callbackUrl: "http://localhost:3000/" });
+  };
 
   console.log(session);
   return (
@@ -22,20 +25,7 @@ const NotLoginPage = () => {
         <s.MewsIntro>지금 뮤즈에 가입하고</s.MewsIntro>
         <s.MewsAd>기깔나는 홍보문구 들어갈 자리</s.MewsAd>
 
-        {/* <s.Button */}
-        {/*  onClick={() => */}
-        {/*    signIn("google", { */}
-        {/*      callbackUrl: "http://localhost:3000/", */}
-        {/*      redirect: false, */}
-        {/*    }) */}
-        {/*  } */}
-        {/* > */}
-        {/*  <GLogo style={{ marginRight: "4%" }} /> */}
-        {/*  Sign in with Google */}
-        {/* </s.Button> */}
-
-        {/* eslint-disable-next-line react/button-has-type */}
-        {!session && <button onClick={() => signIn("google")}>Sign in with Google</button>}
+        {!session && <button onClick={() => getSignIn()}>Sign in with Google</button>}
         {session && (
           <div>
             <p>Signed in as {session.user.email}</p>

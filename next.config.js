533bb05307fdf8ae880238037f6105d17398438f
Choi siun <siun0331@gmail.com>
2023-04-11 15:03:37 +0900

    chore: 빌드환겨 재설정

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
     emotion: true,
   },
   webpack: (config) => {
-    if (isServer) {
-      // 서버 빌드에서 스토리북 코드를 제외합니다.
-      config.module.rules.push({
-        test: [/\.stories\.jsx?$/, /index\.js$/],
-        use: {
-          loader: "ignore-loader",
-        },
-      });
-    }
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],

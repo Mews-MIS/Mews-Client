43fc82693821df0e914a89accb98906c3f6f7a07
Choi siun <siun0331@gmail.com>
2023-04-12 17:57:42 +0900

    refactor: default export add

--- src/pages/search/styles.ts
+++ src/pages/search/styles.ts
@@ -1,7 +1,9 @@
 import styled from "@emotion/styled";
 
 // eslint-disable-next-line import/prefer-default-export
-export const SearchedContainer = styled.div`
+const SearchedContainer = styled.div`
   width: 100%;
   padding: 20px;
 `;
+
+export default SearchedContainer;
